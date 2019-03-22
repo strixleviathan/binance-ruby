fae9dda9ba8c05132982add91d672cda5edbf52a
Jesse Proudman <jesse@jesseproudman.com>
2019-03-22 11:03:11 -0700

    Add missing method

--- lib/binance/api/wapi.rb
+++ lib/binance/api/wapi.rb
@@ -16,6 +16,11 @@ module Binance
           Request.send!(api_key_type: :trading, method: :post, path: path,
                         params: params, security_type: :withdraw)
         end
+
+        private def ensure_required_keys!(params:, required_keys:)
+          missing_keys = required_keys.select { |key| params[key].nil? }
+          raise Error.new(message: "required keys are missing: #{missing_keys.join(', ')}") unless missing_keys.empty?
+        end
       end
     end
   end

e0f30495d796eabdc2f27c900e052c6cc3afe1f0
Jesse Proudman <jesse@jesseproudman.com>
2019-04-08 13:32:06 -0700

    Withdraw history

--- lib/binance/api/wapi.rb
+++ lib/binance/api/wapi.rb
@@ -17,6 +17,18 @@ module Binance
                         params: params, security_type: :withdraw)
         end
 
+        def withdraw_history(asset: nil)
+          timestamp = Binance::Api::Configuration.timestamp
+          params = {
+            asset: asset
+          }.delete_if { |key, value| value.nil? }
+
+          path = "/wapi/v3/withdrawHistory.html"
+          Request.send!(api_key_type: :trading, method: :post, path: path,
+                        params: params, security_type: :withdraw)
+
+        end
+
         private def ensure_required_keys!(params:, required_keys:)
           missing_keys = required_keys.select { |key| params[key].nil? }
           raise Error.new(message: "required keys are missing: #{missing_keys.join(', ')}") unless missing_keys.empty?
